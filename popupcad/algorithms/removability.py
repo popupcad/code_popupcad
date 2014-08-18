26650472f002849aa820d4adc750c916a228e03c
Dan Aukes <danaukes@seas.harvard.edu>
2014-08-18 17:15:00 -0400

    tentative change for modified removability algorithm

--- popupcad/algorithms/removability.py
+++ popupcad/algorithms/removability.py
@@ -6,6 +6,7 @@ Please see LICENSE.txt for full license.
 """
 from popupcad.filetypes.layer import Layer
 from popupcad.materials.laminatesheet import Laminate
+import popupcad.materials.materials as mat
 
 
 def one_way_up(laminatein):
@@ -14,6 +15,7 @@ def one_way_up(laminatein):
     for ii,geoms in enumerate(laminatein):
         l = l.union(geoms)
         laminateout[ii] =l
+    laminateout = modify_up(laminateout)
     return laminateout
 
 def one_way_down(laminatein):
@@ -23,3 +25,11 @@ def two_way(laminatein):
     layers = laminatein.layerdef.layers
     laminateout = laminatein.unarylayeroperation('union',layers,layers)
     return laminateout
+
+def modify_up(removabilityin):
+#    layers = removabilityin.layerdef.layers
+#    for layer1,layer2 in zip(layers[:-1],layers[1:]):
+#        if isinstance(layer1,mat.Adhesive) or isinstance(layer2,mat.Adhesive):
+#            removabilityin.layer_sequence[layer1] = removabilityin.layer_sequence[layer1].union(removabilityin.layer_sequence[layer2])
+    return removabilityin
+    
