89a020a26ea2e006a15e63c3dc0bca25f204ab67
Dan Aukes <danaukes@seas.harvard.edu>
2015-01-12 18:07:29 -0500

    more nonworking changes

--- popupcad_manufacturing_plugins/manufacturing/__init__.py
+++ popupcad_manufacturing_plugins/manufacturing/__init__.py
@@ -8,15 +8,24 @@ Please see LICENSE.txt for full license.
 import types
 import sys
 
+from . import keepout3
+
 from . import manufacturingplugin
+from . import identifybodies2
 from . import identifybodies
+from . import identifyrigidbodies2
 from . import identifyrigidbodies
 from . import customsupport3
+from . import supportcandidate4
 from . import supportcandidate3
 from . import toolclearance2
+from . import toolclearance3
+from . import autoweb4
 from . import autoweb3
 from . import keepout2
+from . import outersheet3
 from . import outersheet2
+from . import removability2
 from . import removability
 from . import scrapoperation
 
