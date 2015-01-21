7a98579794ed65fc53b7ebb700e134e944071904
Aukes, Daniel <danaukes@seas.harvard.edu>
2015-01-21 13:02:31 -0500

    modified script so that dynamic libraries point to the correct location for gfortran.

--- build_mac.sh
+++ build_mac.sh
@@ -1,14 +1,19 @@
-touch libpyside-python2.7.1.2.dylib
-touch libshiboken-python2.7.1.2.dylib
-touch libgeos.3.4.2.dylib
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.special._ufuncs.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.special.specfun.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.linalg._fblas.so
 
-ln -s /Library/Frameworks/QtCore.framework .
-ln -s /Library/Frameworks/QtGui.framework .
-ln -s /Library/Frameworks/QtNetwork.framework .
-ln -s /Library/Frameworks/QtOpenGl.framework .
-ln -s /Library/Frameworks/QtSvg.framework .
-ln -s /Library/Frameworks/QtXml.framework .
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.linalg._flapack.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.integrate._odepack.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.integrate._quadpack.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.integrate.vode.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.integrate._dop.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.integrate.lsoda.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.optimize.minpack2.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.optimize._lbfgsb.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.optimize._cobyla.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.optimize._slsqp.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.optimize._minpack.so
+install_name_tool -change @loader_path/../../../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.sparse.linalg.isolve._iterative.so
+install_name_tool -change @loader_path/../../../../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.sparse.linalg.eigen.arpack._arpack.so
+install_name_tool -change @loader_path/../.dylibs/libgfortran.2.0.0.dylib @loader_path/libgfortran.2.0.0.dylib build/exe.macosx-10.9-x86_64-3.4/scipy.optimize._nnls.so
 
-python build_mac.py bdist_dmg
-
-rm Qt*
