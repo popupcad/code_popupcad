12a35efbcdd7f083b1e574c9603aff12dff23294
Dan Aukes <danaukes@seas.harvard.edu>
2015-07-01 14:11:49 -0400

    added upgrade function to materials

--- popupcad/materials/materials.py
+++ popupcad/materials/materials.py
@@ -24,6 +24,11 @@ class Material(object):
 
     def __repr__(self):
         return str(self)
+
+    def upgrade(self):
+        from popupcad.filetypes.material2 import Material2
+        new = Material2(self.name,self.color,self.thickness,E1 = 1,E2 = 1,poisson = .5,self.is_adhesive,self.is_rigid,False)
+        return new
     
 class Carbon_0_90_0(Material):
     is_rigid = True
