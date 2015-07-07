38b13abba07a3df67209bcf605b077ade54e44fb
Dan Aukes <danaukes@seas.harvard.edu>
2015-07-07 16:03:28 -0400

    moved save_dxf back where it belongs

--- popupcad/filetypes/genericlaminate.py
+++ popupcad/filetypes/genericlaminate.py
@@ -91,6 +91,19 @@ class GenericLaminate(popupCADFile):
         filename_out = gv.raster(destination, filename, filetype)
         return filename_out
         
+    def save_dxf(self,filename):
+        import ezdxf
+        
+        dwg = ezdxf.new('AC1015')
+        msp = dwg.modelspace()
+
+        for layer in self.layerdef.layers:
+            dxf_layer = dwg.layers.create(name=layer.id)
+            for item in self.geoms[layer]:
+                if not item.is_construction():
+                    item.output_dxf(msp,layer.id)
+        
+        dwg.saveas(filename)                
         
     #Returns the thickness of the laminate
     def getLaminateThickness(self):
@@ -238,18 +251,3 @@ class GenericLaminate(popupCADFile):
         geom.primitives.append(triset)
         return geom
 
-
-if __name__ == '__main__':   
-    def save_dxf(self,filename):
-        import ezdxf
-        
-        dwg = ezdxf.new('AC1015')
-        msp = dwg.modelspace()
-
-        for layer in self.layerdef.layers:
-            dxf_layer = dwg.layers.create(name=layer.id)
-            for item in self.geoms[layer]:
-                if not item.is_construction():
-                    item.output_dxf(msp,layer.id)
-        
-        dwg.saveas(filename)                
