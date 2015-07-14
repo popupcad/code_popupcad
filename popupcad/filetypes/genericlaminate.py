2c3ec9092dbd21fb269715a4cc77ccfedd4356cd
Dan Aukes <danaukes@seas.harvard.edu>
2015-07-14 14:34:49 -0400

    fixed center of mass calc

--- popupcad/filetypes/genericlaminate.py
+++ popupcad/filetypes/genericlaminate.py
@@ -287,18 +287,22 @@ class GenericLaminate(popupCADFile):
         volume_total = 0
         center_of_mass_accumulator = 0
         next_args = []
+        mass_total = 0
         for layer in self.layers():
+            layer_volume = 0
             density = layer.density
             z_lower = zvalues[layer]['lower']
             z_upper = zvalues[layer]['upper']
             for geom in self.geoms[layer]:
                 area,centroid,volume,mass,tris = geom.mass_properties(density,z_lower,z_upper,length_scaling)
                 volume_total+=volume
-                center_of_mass_accumulator+=volume*centroid
+                layer_volume+=volume
+                center_of_mass_accumulator+=volume*centroid*density
                 next_args.append((geom,(density,z_lower,z_upper,tris)))
-        center_of_mass = center_of_mass_accumulator/volume_total
+            mass_total+=layer_volume*density
+        center_of_mass = center_of_mass_accumulator/mass_total
         I = 0
         for geom,args in next_args:
             I+=geom.inertia_tensor(center_of_mass,*args)
-        return volume_total,center_of_mass,I
+        return volume_total,mass_total,center_of_mass,I
             
