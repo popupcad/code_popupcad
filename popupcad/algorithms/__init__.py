0606f7051701b44c7c59e8684e83a06328c6e606
Dan Aukes <danaukes@seas.harvard.edu>
2015-10-23 17:32:11 -0700

    added new modules

--- popupcad/algorithms/__init__.py
+++ popupcad/algorithms/__init__.py
@@ -12,11 +12,16 @@ from . import getjoints
 from . import keepout
 from . import manufacturing_functions
 from . import minimal_enclosing_circle
+from . import modify_device
 from . import morphology
 from . import painterpath
 from . import points
 from . import python_syntax_formatter
+from . import removability
 from . import spline_functions
 from . import tetrahedron
+from . import toolclearance
 from . import triangle
 from . import triangulate
+from . import web
+
