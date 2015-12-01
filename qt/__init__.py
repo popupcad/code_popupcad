3c152e16f5628c67e3099964f0909b70dedc775f
Dan Aukes <danaukes@seas.harvard.edu>
2015-11-30 17:57:50 -0800

    initial commit for pyqt5

--- qt/__init__.py
+++ qt/__init__.py
@@ -11,7 +11,14 @@ if '--pyqt4' in argv:
     loaded = 'PyQt4'
     pyqt_loaded = True
     pyside_loaded = False
+    pyqt5_loaded = False
+elif '--pyqt5' in argv:
+    loaded = 'PyQt5'
+    pyqt_loaded = False
+    pyside_loaded = False
+    pyqt5_loaded = True    
 else:
     loaded = 'PySide'
     pyqt_loaded = False
     pyside_loaded = True
+    pyqt5_loaded = False
