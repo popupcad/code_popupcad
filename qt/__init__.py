6cac18bf345ea2f13ce2b06d28f02e563f419f61
Dan Aukes <danaukes@asu.edu>
2016-03-17 12:06:41 -0700

    fixed filetype checking bug in sketch
    fixed qt checking code

--- qt/__init__.py
+++ qt/__init__.py
@@ -7,9 +7,9 @@ Created on Wed Oct 28 11:06:44 2015
 
 import sys
 argv = [item.lower() for item in sys.argv]
-if '--pyqt4' in argv:
+if 'qt4' in argv:
     loaded = 'PyQt4'
-elif '--pyqt5' in argv:
+elif 'qt5' in argv:
     loaded = 'PyQt5'
 else:
     loaded = 'PySide'
