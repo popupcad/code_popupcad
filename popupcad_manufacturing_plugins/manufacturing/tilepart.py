eb88ecb9822130ca1e5eff17672b044b2ee5606f
Gravish, Nick <nickgravish@Nicks-iMac.local>
2016-03-21 13:22:28 -0400

    Added documentation to select interface

--- popupcad_manufacturing_plugins/manufacturing/tilepart.py
+++ popupcad_manufacturing_plugins/manufacturing/tilepart.py
@@ -10,6 +10,8 @@ Please see LICENSE for full license.
 # import sys
 # import numpy as np
 import math
+from platform import release
+
 import qt.QtGui as qg
 
 # from popupcad.filetypes.operationoutput import OperationOutput
@@ -27,6 +29,7 @@ from popupcad.manufacturing.multivalueoperation3 import MultiValueOperation3
 from popupcad.widgets.dragndroptree import DraggableTreeWidget
 from popupcad.widgets.listmanager import SketchListManager,SketchListViewer
 from popupcad.filetypes.operationoutput import OperationOutput
+from popupcad.filetypes.laminate import Laminate
 
 class Dialog(qg.QDialog):
 
@@ -36,8 +39,10 @@ class Dialog(qg.QDialog):
         if tilepart is not None:
 
             self.sheet_opref_tp = tilepart.sheet_opref
+            self.release_opref_tp = tilepart.release_opref
             self.part_opref_tp = tilepart.part_opref
             self.sketch_bounding_box_tp = tilepart.sketch_bounding_box
+            self.sketch_id_tp = tilepart.sketch_id
             self.sc_tp = tilepart.sc
             self.N_tp = tilepart.N
             self.x_gap_tp = tilepart.x_gap
@@ -46,8 +51,13 @@ class Dialog(qg.QDialog):
 
         else:
             # define defaults
-            self.sheet_opref_tp = None
-            self.part_opref_tp = None
+            self.sheet_opref_tp = None if len(design.operations) == 0 else design.operations[0].id
+            self.release_opref_tp = None if len(design.operations) == 0 else design.operations[0].id
+            self.part_opref_tp = None if len(design.operations) == 0 else design.operations[0].id
+
+            sketchids = [id for key, id in enumerate(design.sketches)]
+            self.sketch_id_tp = None if len(design.sketches) == 0 else design.sketches[sketchids[0]]
+
             self.sketch_bounding_box_tp = [(0,0),(0,0)]
             self.sc_tp = 1
             self.N_tp = 10
@@ -69,20 +79,19 @@ class Dialog(qg.QDialog):
 
         self.part = DraggableTreeWidget()
         self.part.linklist(prioroperations)
-        self.part.setObjectName("Operation to tile")
-        # self.part.setCurrentItem(design.operation_index(self.part_opref_tp))
+        # self.part.setCurrentItem()
+        self.part.selectIndeces([(design.operation_index(self.part_opref_tp), 0)])
 
         self.release = DraggableTreeWidget()
         self.release.linklist(prioroperations)
-        self.release.setObjectName("Release cut (optional)")
-        # self.part.setCurrentItem(design.operation_index(self.part_opref_tp))
+        self.release.selectIndeces([(design.operation_index(self.release_opref_tp), 0)])
 
         self.sketch_to_tile = SketchListManager(design,name = 'Sketch of tile area')
+        self.sketch_to_tile.refresh_list(selected_item = self.sketch_id_tp)
 
         self.sheet = DraggableTreeWidget()
         self.sheet.linklist(prioroperations)
-        self.sheet.setObjectName("Sheet to tile into")
-        # self.sheet.setCurrentItem(design.operation_index(self.sheet_opref_tp))
+        self.sheet.selectIndeces([(design.operation_index(self.sheet_opref_tp), 0)])
 
         #       operation/part | sketch to tile in | sheet to tile into
         layout_ops_sheet_sketch = qg.QHBoxLayout()
@@ -91,6 +100,29 @@ class Dialog(qg.QDialog):
         layout_ops_sheet_sketch.addWidget(self.sketch_to_tile)
         layout_ops_sheet_sketch.addWidget(self.sheet)
 
+        # layout_ops_sheet_sketch = qg.QHBoxLayout()
+        # part_vbox = qg.QVBoxLayout()
+        # part_vbox.addWidget(qg.QLabel('Part to tile'))
+        # part_vbox.addWidget(self.part)
+        # layout_ops_sheet_sketch.addLayout(part_vbox)
+        # layout_ops_sheet_sketch.addStretch()
+        #
+        # rel_vbox = qg.QVBoxLayout()
+        # rel_vbox.addWidget(qg.QLabel('Release cut (optional)'))
+        # rel_vbox.addWidget(self.release)
+        # layout_ops_sheet_sketch.addLayout(rel_vbox)
+        # layout_ops_sheet_sketch.addStretch()
+        #
+        # sktch_vbox = qg.QVBoxLayout()
+        # sktch_vbox.addWidget(self.sketch_to_tile)
+        # layout_ops_sheet_sketch.addLayout(sktch_vbox)
+        # layout_ops_sheet_sketch.addStretch()
+        #
+        # sht_vbox = qg.QVBoxLayout()
+        # sht_vbox.addWidget(qg.QLabel('Sheet to tile into'))
+        # sht_vbox.addWidget(self.release)
+        # layout_ops_sheet_sketch.addLayout(sht_vbox)
+
         #       'Number of parts', 'Scale'
         number_of_parts_and_scale = qg.QHBoxLayout()
 
@@ -98,13 +130,11 @@ class Dialog(qg.QDialog):
         number_of_parts_and_scale.addWidget(qg.QLabel('Number of parts'))
         self.N = qg.QLineEdit()
         number_of_parts_and_scale.addWidget(self.N)
-        self.N.setText(str(self.N_tp))
 
         number_of_parts_and_scale.addWidget(qg.QLabel('Scale'))
         self.scale = qg.QLineEdit()
         number_of_parts_and_scale.addWidget(self.scale)
         number_of_parts_and_scale.addStretch()
-        self.scale.setText(str(self.sc_tp))
 
         #                       'x-gap', 'y-gap'
         xy_gap = qg.QHBoxLayout()
@@ -112,12 +142,10 @@ class Dialog(qg.QDialog):
 
         xy_gap.addWidget(qg.QLabel('X-gap'))
         self.x_gap = qg.QLineEdit()
-        self.x_gap.setText(str(self.x_gap_tp))
         xy_gap.addWidget(self.x_gap)
 
         xy_gap.addWidget(qg.QLabel('Y-gap'))
         self.y_gap = qg.QLineEdit()
-        self.y_gap.setText(str(self.y_gap_tp))
         xy_gap.addWidget(self.y_gap)
         xy_gap.addStretch()
 
@@ -125,8 +153,6 @@ class Dialog(qg.QDialog):
         s_offset.addStretch()
         s_offset.addWidget(qg.QLabel('Support offset'))
         self.support_offset = qg.QLineEdit()
-        self.support_offset.setText(str(self.support_offset_tp))
-        s_offset.addWidget(self.support_offset)
         s_offset.addWidget(self.support_offset)
         s_offset.addStretch()
 
@@ -140,8 +166,15 @@ class Dialog(qg.QDialog):
         button2.clicked.connect(self.reject)
         buttons.addWidget(button2)
 
+        instructions = qg.QHBoxLayout()
+        instructions.addStretch()
+        instructions.addWidget(qg.QLabel("Part to tile | Release cut (optional) | "
+                                                               "Bounding box sketch | Sheet to tile into"))
+        instructions.addStretch()
+
 
         layout = qg.QVBoxLayout()
+        layout.addLayout(instructions)
         layout.addLayout(layout_ops_sheet_sketch)
         layout.addLayout(number_of_parts_and_scale)
         layout.addLayout(xy_gap)
@@ -149,6 +182,12 @@ class Dialog(qg.QDialog):
         layout.addLayout(buttons)
         self.setLayout(layout)
 
+        # set text of all boxes
+        self.scale.setText(str(self.sc_tp))
+        self.N.setText(str(self.N_tp))
+        self.y_gap.setText(str(self.y_gap_tp))
+        self.x_gap.setText(str(self.x_gap_tp))
+        self.support_offset.setText(str(self.support_offset_tp))
 
     def build_sketch_links(self):
         try:
@@ -176,9 +215,12 @@ class Dialog(qg.QDialog):
         release_opref = opid
 
         # get bounding box from the sketch
-        sketch_id = self.sketch_to_tile.itemlist.selectedItems()[0].value.id
-        sketch_bounding_box = self.design.sketches[sketch_id].output_csg()[0].bounds # may break if multiple sketches
+        tmp_sketch_id = self.sketch_to_tile.itemlist.selectedItems()[0].value.id
+        sketch_bounding_box = self.design.sketches[tmp_sketch_id].output_csg()[0].bounds # may break if multiple sketches
         sketch_bounding_box = [geom/popupcad.csg_processing_scaling for geom in sketch_bounding_box]
+
+        sketch_id = self.sketch_to_tile.itemlist.selectedItems()[0].value
+
 
         N = int(self.N.text())
         scale = float(self.scale.text())
@@ -186,7 +228,8 @@ class Dialog(qg.QDialog):
         y_gap = float(self.y_gap.text())
         support_offset = float(self.support_offset.text())
 
-        return part_opref, release_opref, sheet_opref, sketch_bounding_box, N, scale, x_gap, y_gap, support_offset
+        return part_opref, release_opref, sheet_opref, sketch_bounding_box, N, scale, \
+               x_gap, y_gap, support_offset, sketch_id
 
 
 class TilePart(Operation2):
@@ -198,12 +241,14 @@ class TilePart(Operation2):
         self.editdata(*args)
         self.id = id(self)
 
-    def editdata(self,part_opref, release_opref, sheet_opref, sketch_bounding_box, N, scale, x_gap, y_gap, support_offset):
+    def editdata(self,part_opref, release_opref, sheet_opref, sketch_bounding_box, N, scale, x_gap, y_gap,
+                 support_offset, sketch_id):
         super(TilePart, self).editdata({},{},{})
         self.sheet_opref = sheet_opref
         self.release_opref = release_opref
         self.part_opref = part_opref
         self.sketch_bounding_box = sketch_bounding_box
+        self.sketch_id = sketch_id
         self.sc = scale
         self.N = N
         self.x_gap = x_gap
@@ -220,7 +265,8 @@ class TilePart(Operation2):
             self.sc,
             self.x_gap,
             self.y_gap,
-            self.support_offset)
+            self.support_offset,
+            self.sketch_id)
         new.id = self.id
         new.customname = self.customname
         return new
@@ -264,15 +310,6 @@ class TilePart(Operation2):
         # add sketch to sketch list
         design_copy.sketches[construction_geom_hinge.id] = construction_geom_hinge
 
-        # # make the sketchop
-        # construction_geom_sketchop_hinge = SimpleSketchOp({'sketch': [construction_geom_hinge.id]},
-        #                                     [layer.id for layer in design_copy.return_layer_definition().layers])
-        # construction_geom_sketchop_hinge.name = "ConstructionLine"
-        #
-        # # finally initialize sketch op in design_copy
-        # design_copy.addoperation(construction_geom_sketchop_hinge)
-        # construction_geom_sketchop_hinge.generate(design_copy)
-
         ######################## generate the external transform geometry in the sheet
 
         # center the locate line top left as origin
@@ -288,21 +325,20 @@ class TilePart(Operation2):
         max_num_cols = divmod(self.sketch_bounding_box[2] - self.sketch_bounding_box[0], width)[0]
         max_num_rows = divmod(self.sketch_bounding_box[3] - self.sketch_bounding_box[1], height)[0]
 
-        arrayed_reference_lines = []
+        if max_num_cols == 0 or max_num_rows == 0:
+            print('Cannot tile into this area')
+            design.remove_operation(new_web)
+            return Laminate()
 
         # check if can fit in one
         # if N <= max_num_rows*max_num_cols:
         rows = math.ceil(self.N / max_num_cols)
         cols = math.ceil(self.N / rows)          # spread across the two windows
 
-
-        # new_center = (-parts_bounding_box[0]/2, 2)
-        # tmp_geom = [(x + new_center[0], y + new_center[1]) for (x,y) in tmp_geom]
-
         upper_right_origin_bounding_box = (self.sketch_bounding_box[0], self.sketch_bounding_box[3])
 
         n_count = 0
-
+        arrayed_reference_lines = []
         for row in range(rows):
             for col in range(cols):
                 if n_count >= self.N or n_count >= max_num_rows*max_num_cols*2:
@@ -322,15 +358,6 @@ class TilePart(Operation2):
 
         # add sketch to sketch list
         design_copy.sketches[construction_geom_sheet.id] = construction_geom_sheet
-
-        # # make the sketchop
-        # construction_geom_sketchop_sheet = SimpleSketchOp({'sketch': [construction_geom_sheet.id]},
-        #                                     [layer.id for layer in design_copy.return_layer_definition().layers])
-        # construction_geom_sketchop_sheet.name = "ConstructionLine"
-
-        # # finally initialize sketch op in design_copy
-        # design_copy.addoperation(construction_geom_sketchop_sheet)
-        # construction_geom_sketchop_sheet.generate(design_copy)
 
         ######################## External transform the hinge onto the sheet construction line
 
