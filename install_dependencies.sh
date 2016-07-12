533d09ffc864b0d857aea573152bcc42b0767bc5
Dan Aukes <danaukes@asu.edu>
2016-07-12 14:15:23 -0700

    updated travis build

---
+++ install_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install libgeos-dev gfortran cmake python-qt4-gl phonon-backend-gstreamer
+sudo apt-get install python3-pip spyder3 python3-pyside python3-shapely python3-yaml python3-scipy python3-setuptools cython3 python3-dev python3-sphinx python3-pyqt4.qtopengl
+sudo pip3 install sympy pycollada ezdxf numpy-stl lxml pyopengl pyopengl_accelerate pyqtgraph pypoly2tri#
