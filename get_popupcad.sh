1781a6e82d61f56446b559ea390e572146068578
Dan Aukes <danaukes@asu.edu>
2016-07-13 13:22:40 -0700

    updated travis and dependencies install.

---
+++ get_popupcad.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install -y git
+git config --global user.name "LastName, Firstname"
+git config --global user.email "email@address.com"
+cd ~
+git clone https://github.com/popupcad/popupcad.git
+cd ~/popupcad
+git checkout pyqt4_only
+git pull
+echo "export PYTHONPATH=\$PYTHONPATH:~/popupcad" >> ~/.bashrc
+source ~/.bashrc
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
-sudo apt-get install libgeos-dev gfortran cmake python-qt4-gl phonon-backend-gstreamer
-sudo apt-get install python3-pip spyder3 python3-pyside python3-shapely python3-yaml python3-scipy python3-setuptools cython3 python3-dev python3-sphinx python3-pyqt4.qtopengl
-sudo pip3 install sympy pycollada ezdxf numpy-stl lxml pyopengl pyopengl_accelerate pyqtgraph pypoly2tri#
+sudo apt-get update
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda create -q -n popupcad_env python=$TRAVIS_PYTHON_VERSION shapely pyyaml numpy scipy setuptools cython pyqt sympy pyopengl pyqtgraph matplotlib
+pip install pypoly2tri ezdxf
+source activate popupcad_env
+python setup.py install
