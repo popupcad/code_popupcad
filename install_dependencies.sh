c379588c72332e436b7789426f43d476bada13bc
LastName, Firstname <email@address.com>
2016-07-15 15:59:41 -0700

    added conditional to check for travis

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [[ "$TRAVIS_PYTHON_VERSION" == "" ]]; then
+  export TRAVIS_PYTHON_VERSION=3.5;
+fi
+
 sudo apt-get update
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
