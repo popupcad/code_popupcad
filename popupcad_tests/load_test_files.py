17083a1476473d26f757d446f5a877f79f686ac1
LastName, Firstname <email@address.com>
2016-07-15 17:26:30 -0700

    added app because text op requires it.

--- popupcad_tests/load_test_files.py
+++ popupcad_tests/load_test_files.py
@@ -4,14 +4,18 @@ Written by Daniel M. Aukes and CONTRIBUTORS
 Email: danaukes<at>asu.edu.
 Please see LICENSE for full license.
 """
-
+import sys
 import os
 import popupcad
 import time
 import glob
 import time
+import qt.QtCore as qc
+import qt.QtGui as qg
+
 
 if __name__=='__main__':
+    app = qg.QApplication([sys.argv[0]])
     t_0 = time.time()
     top_directory = popupcad.test_file_dir
     filenames = []
@@ -26,6 +30,7 @@ if __name__=='__main__':
 #        print(filename)
         full_filename = os.path.normpath(os.path.join(directory,filename))
         try:
+            print(full_filename)
             d = popupcad.filetypes.design.Design.load_yaml(full_filename)
             d.reprocessoperations()
             passed.append(full_filename)
